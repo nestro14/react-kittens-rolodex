af80691ef4010137a895b739157ba39d9924a259
Nestor Ceron <nestor_ceron@yahoo.com>
2020-06-15 23:09:57 -0700

    Updates

---
+++ precache-manifest.8b014b86eef997355046f15b47eb825d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4488a3a7175fe4b0730f4c0434de392c",
+    "url": "/react-kittens-rolodex/index.html"
+  },
+  {
+    "revision": "53934ce4a758fa191f52",
+    "url": "/react-kittens-rolodex/static/css/main.64b18e24.chunk.css"
+  },
+  {
+    "revision": "fc762f12bae81b8ce5f8",
+    "url": "/react-kittens-rolodex/static/js/2.304bc2ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-kittens-rolodex/static/js/2.304bc2ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53934ce4a758fa191f52",
+    "url": "/react-kittens-rolodex/static/js/main.b8798cdc.chunk.js"
+  },
+  {
+    "revision": "2904b18ed11a9f2af524",
+    "url": "/react-kittens-rolodex/static/js/runtime-main.e0c9d892.js"
+  }
+]);
